b723525333a0078eace94318058c7243594a82cb
SaajanM <saajan.maslanka@gmail.com>
2017-08-13 18:29:18 -0700

    Started adding content to points page

--- app/src/main/java/com/cusd80/cchs/roboticsclub/MainActivity.java
+++ app/src/main/java/com/cusd80/cchs/roboticsclub/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
         mEmailView=(TextView) mHeaderView.findViewById(R.id.emailView);
-        mEmailView.setHeight(mEmailView.getHeight()+100);
+        //mEmailView.setHeight(mEmailView.getHeight()+100);
         mUsernameView=(TextView) mHeaderView.findViewById(R.id.usernameView);
 
         Intent intent = getIntent();
@@ -44,15 +44,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -77,45 +68,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_points) {
+            onBackPressed();
+        } else if (id == R.id.nav_idea) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_acct) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_admin) {
 
         }
 
